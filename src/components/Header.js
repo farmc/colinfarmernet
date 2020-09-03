30a48afebb00ab0665aee8894460071c1b43f024
Colin Farmer <farmerc@umich.edu>
2020-09-03 12:54:43 -0400

    nav bar works on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation  } from 'react-router-dom';
 import HeaderItem from './HeaderItem.js';
+import menuwhite from '../images/menuwhite.png'
+import menublack from '../images/menublack.png'
 
 function Header(){
 
@@ -9,6 +11,10 @@ function Header(){
     let location = useLocation();
     const [textColor, setTextColor] = useState(String(location.pathname) !== "/" ? 'black' : 'white');
 
+    //Show menu on mobile devices
+    const [showMenu, setShowMenu] = useState(false);
+
+    //check location for text color
     useEffect(() => {
         if (String(location.pathname) !== "/"){
             setTextColor('black');
@@ -19,6 +25,33 @@ function Header(){
         
     }, [location.pathname]);
 
+
+    //nav bar menu
+    let mdmenu =  
+        <div className="md:flex">
+            <div onClick={() => {setTextColor('black'); setShowMenu(false)}} className="md:flex px-2 md:pl-8 md:pr-4">
+                <HeaderItem text="Video" loc="/video" color={textColor} />
+            </div>
+            <div onClick={() => {setTextColor('black'); setShowMenu(false)}} className="md:flex px-2 md:pl-8 md:pr-4">
+                <HeaderItem text="Software" loc="/software" color={textColor} onClick={() => setTextColor('black')} />
+            </div>
+            <div onClick={() => {setTextColor('black'); setShowMenu(false)}} className="md:flex px-2 md:pl-8 md:pr-4">
+                <HeaderItem text="About" loc="/about" color={textColor} onClick={() => setTextColor('black')}/>
+            </div>
+        </div>
+
+    //variable for mobile menu
+    let menu;
+
+    //set if showMenu is activated
+    if(showMenu){
+        menu = mdmenu
+    }
+
+    
+    //variable for switching between black/white menu bar pngs
+    let menucolor = (textColor === "white" ? menuwhite : menublack);
+
     
 
 
@@ -28,7 +61,7 @@ function Header(){
             <nav className="p-4">
 
                 {/* left side of nav bar (name, video, software, about)*/}
-                <div className="sm:block md:flex lg:flex xl:flex items-baseline">
+                <div className="flex justify-between items-start md:justify-start md:items-baseline md:">
 
                     <Link 
                         to="/" 
@@ -37,16 +70,17 @@ function Header(){
                     >
                         Colin Farmer
                     </Link>
-            
-                    <div onClick={() => setTextColor('black')} className="md:pl-8 md:pr-4">
-                        <HeaderItem text="Video" loc="/video" color={textColor} />
+
+                    <div className="block text-right md:text-left md:flex">
+                        <div className="md:hidden inline-block right-0" onClick={() => setShowMenu(!showMenu) }>
+                            <img className="h-8 w-8" src={menucolor} alt="Menu"/>
+                        </div>
+                        {menu}
+                        <div className="hidden md:flex">
+                            {mdmenu}
+                        </div>
                     </div>
-                    <div onClick={() => setTextColor('black')} className="md:px-4">
-                        <HeaderItem text="Software" loc="/software" color={textColor} onClick={() => setTextColor('black')} />
-                    </div>
-                    <div onClick={() => setTextColor('black')} className="md:px-4">
-                        <HeaderItem text="About" loc="/about" color={textColor} onClick={() => setTextColor('black')}/>
-                    </div>
+                    
                     
                 </div>
             </nav>
