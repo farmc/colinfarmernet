7836fd257f14eecb184eb5d250f4632e91b1a290
Colin Farmer <farmerc@umich.edu>
2020-09-03 09:40:05 -0400

    moved animations

--- src/components/HeaderItem.js
+++ src/components/HeaderItem.js
@@ -3,20 +3,13 @@ import { Link } from 'react-router-dom';
 
 function HeaderItem(props){
 
-    //const [isHovering, setIsHovering] = useState(false);
-
-    //const animate = useSpring({transform: isHovering ? 200 : 100})
-
     return(
-        //<animated.div style={animate}>
             <Link
                 to={props.loc}
                 className={"text-" + props.color + " hover:text-gray-300"} 
-                //onMouseOver={() => setIsHovering(true)}
             >
                 {props.text}
             </Link>
-        //</animated.div>
     )
 
 
