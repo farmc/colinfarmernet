ca483787ea20733df41ecf3adbd0218d92f52278
Colin Farmer <farmerc0926@gmail.com>
2022-10-14 22:28:50 -0400

    updated link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import linkedin from '../images/linkedin.png';
-import github from '../images/github.png';
+import React from "react";
+import linkedin from "../images/linkedin.png";
+import github from "../images/github.png";
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <footer className="flex text-xs p-4 absolute bottom-0 w-full justify-between text-white items-center">
-            <div className="ml-3">
-                &copy; 2021
-            </div>
-            <div className="flex" >
-                <a href="http://www.linkedin.com/in/cfarm" target="_blank" rel="noopener noreferrer">
+            <div className="ml-3">&copy; 2021</div>
+            <div className="flex">
+                <a
+                    href="http://www.linkedin.com/in/cfarm"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img src={linkedin} alt="" className="h-6 w-6 " />
                 </a>
-                <a href="http://www.github.com/farmerc0926" target="_blank" rel="noopener noreferrer"> 
+                <a
+                    href="http://www.github.com/farmc"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img src={github} alt="" className="h-6 w-6 ml-3 mr-3" />
                 </a>
             </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
